26b1d235c3fe689e30dc4be7e5d798f7b2686f54
M0th4Fuck4 <M0th4Fuck4@protonmail.com>
2026-09-20 19:14:48 +0000

    Update uvicorn port configuration and add DOORWATCHER_PORT to .env.example

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ async def recording():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8801, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=settings.get_settings().get("DOORWATCHER_PORT"), reload=True)
